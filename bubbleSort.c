5486cb22b4127a88d5ab8f9e6ae743c227b54d38
Mohammed BOUSSARDI <66888805+medbsrdi@users.noreply.github.com>
2021-12-08 10:29:42 +0100

    Bubble sort

---
+++ bubbleSort.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+#define SIZE 10
+
+int main(void)
+{
+	int numbers[SIZE];
+	int i,outer,inner,tmp;
+	// populate the array 
+	srand((unsigned)time(NULL));
+	for(i = 0 ; i < SIZE ; i++)
+		numbers[i] = rand()%100 + 1;
+	// Display the unsorted array
+	printf("unsorted Array :");
+	for(i = 0 ; i < SIZE ; i++)
+		printf(" %3d",numbers[i]);
+	putchar('\n');
+	// Sort the array 
+	for(outer = 0 ; outer < SIZE ; outer++)
+	{
+		for(inner = outer+1 ; inner < SIZE ; inner++)
+		{
+			if(numbers[outer] > numbers[inner])
+			{
+				 tmp = numbers[inner];
+				 numbers[inner] = numbers[outer];
+				 numbers[outer] = tmp;
+			}
+		}
+	}
+	// Display sorted array
+	printf("Sorted array :");
+	for(i = 0 ; i < SIZE ; i++)
+		printf(" %3d",numbers[i]);
+	putchar('\n');
+	return 0;
+}
