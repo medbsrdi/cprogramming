40f40ea68d8708f773ce80be674f353b325f4cfa
Mohammed BOUSSARDI <66888805+medbsrdi@users.noreply.github.com>
2021-12-27 06:00:21 +0100

    upgrade version adding new function
    Insert in a sorted list problem with negative value should be corrected

--- ArrayAbstractDataType.c
+++ ArrayAbstractDataType.c
@@ -173,14 +173,173 @@ int RBinarySearch(Array a,int l,int h,int elm)
 	}
 }
 
+// Get(index)
+// first checking the valid index
+// if(index>= 0 && index < length)
+// time complexity is constent
+// set(index , value) => same as get operation
+// max(array) => traversing all the element to get the max
+// Recursive Sum(array,length) => A[n] + Sum(A,n-1);
+
+
+int Get(int index,Array a)
+{
+	if(index >= 0 && index <a.length)
+	{
+		return a.A[index];
+	}
+	return INT_MIN;
+}
+
+void Set(int index,int value,Array *a)
+{
+	if(index >= 0 && index < a->length){
+		a->A[index] = value;
+	}else{
+		printf("Invalid index \n");
+	}	
+}
+
+int Max(const Array arr)
+{
+	int max = arr.A[0];
+	int i;
+	for(i = 1 ; i < arr.length ; i++)
+	{
+		if(arr.A[i] > max)
+		{
+			max = arr.A[i];
+		}
+	}
+	return max;
+}
+
+int Min(Array arr)
+{
+	int min = arr.A[0];
+	int i;
+	for(i = 1 ; i < arr.length ; i++)
+	{
+		if(arr.A[i] < min)
+		{
+			min = arr.A[i];
+		}
+	}
+	return min;
+}
+
+int Sum(Array arr)
+{
+	int sum = 0;
+	int i;
+	for(i = 0 ; i < arr.length ; i++)
+	{
+		sum += arr.A[i];
+	}
+	return sum;
+}
+
+double Avg(Array arr)
+{
+	return (double)Sum(arr)/arr.length;
+}
+
+// Reversing the element of an array
+// 1st method creating an auxilary array storing from last index to it
+// 2st scan from two ends of an array and interchange the element 
+//Shifting / Rotate
+// Shifting by 1:
+// ABCDE --> BCDE0
+// Rotate by 1:
+// ABCDE --> BCDEA
+
+void Reverse(Array *arr)
+{
+	int B[arr->length];
+	int i;
+	for(i = arr->length-1 ; i >=0 ; i--)
+	{
+		B[i] = arr->A[arr->length-i-1];
+	}
+	for(i = 0 ; i < arr->length ; i++)
+	{
+		arr->A[i] = B[i];
+	}
+}
+
+void ReverseT(Array *arr)
+{
+	int i,j;
+	for(i = 0 , j = arr->length-1 ; i < j ; i++ , j--)
+	{
+		swap(&arr->A[i],&arr->A[j]);
+	}
+}
+
+void leftShift(Array *arr)
+{
+	int i,tmp;
+	for(i = 0 ; i < arr->length-1 ; i++)
+		arr->A[i] = arr->A[i+1];
+	arr->A[arr->length-1] = 0;
+}
+
+
+
+void leftRotate(Array *arr)
+{
+	int i,tmp;
+	tmp = arr->A[0];
+	leftShift(arr);
+	arr->A[arr->length-1] = tmp;	
+}
+
+
+void RightShift(Array *arr)
+{
+	int i;
+	for(i = arr->length -1 ; i > 0 ; i--)
+	{
+		arr->A[i] = arr->A[i-1];
+	}
+	arr->A[0] = 0;
+}
+
+void RightRotate(Array *arr)
+{
+	int i,tmp;
+	tmp = arr->A[arr->length-1];
+	RightShift(arr);
+	arr->A[0] = tmp;
+}
+
+// Inserting in a sorted Array 
+// Checking if an Array is Sorted
+// Arranging -elms on left Side
+
+void InsertingSorted(Array *arr,int elm)
+{
+	int i;
+	i = arr->length-1;
+	while(arr->A[i] > elm)
+	{
+		arr->A[i+1] = arr->A[i];
+		i--;
+	}
+	arr->A[i+1] = elm;
+	arr->length++;
+}
+
 
 int main(void)
 {
-	Array arr = {{1,3,4,5},4,10};
+	Array arr = {{1,3,7},3,10};
 	Append(&arr,9);
 	Display(arr);
-	int b = RBinarySearch(arr,0,arr.length-1,1);
-	printf("5 is int the position %d",b);
+	InsertingSorted(&arr,-34);
+	Display(arr);
+	
+	
 	
 	
 	return 0;
